4cbd8d878d71c29c465bb9b98224a5323c14e27e
chiefJurist <anthonynnanna004@gmail.com>
2024-04-12 20:02:32 +0100

    update

--- PART 6/Inheritance.sol
+++ PART 6/Inheritance.sol
@@ -16,7 +16,50 @@ contract Ownable {
     constructor () {
         owner = msg.sender;
     }
+}// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
 }
+
+contract SecretVault {
+    string private secret;
+
+    constructor(string memory _secret) {
+        secret = _secret;
+    }
+
+    function getSecret() public view returns (string memory) {
+        return secret;
+    }
+}
+
+contract MyContract is Ownable {
+    address private secretVault;
+
+    constructor(string memory _secret) {
+        // Create a new SecretVault contract
+        SecretVault _secretVault = new SecretVault(_secret);
+        // Store the address of the created SecretVault contract
+        secretVault = address(_secretVault);
+    }
+
+    function getSecret() public view onlyOwner returns (string memory) {
+        // Access the SecretVault contract using the stored address
+        return SecretVault(secretVault).getSecret();
+    }
+}
+
 
 //A factory (e.g. SecretVault) is a smart contract that creates other smart contracts
 
